d591bb1d19af811ebdd4dd5644b0345a34caeaa7
Alec Story <avs38@cornell.edu>
2011-03-15 23:29:41 -0400

    app

--- typechecker.ml
+++ typechecker.ml
@@ -28,53 +28,83 @@
 (* Type checking for the Frenetic syntax                                      *)
 (* $Id$ *)
 (******************************************************************************)
+module StringMap = Map.Make (String)
 include Syntax
-open Id
-open Map
-
-module StringMap = Map.Make (
-  struct
-    type t = string
-    let compare = compare
-  end )
 module ConstraintSet = Set.Make (
   struct
     let compare = compare
-    type t = typ
+    type t = typ * typ
   end )
 
 exception TypeException of (Info.t * string)
 
+type gammat = typ StringMap.t 
+type constrt = ConstraintSet.t
+
+(* TODO(astory): make this actually produce fresh variables *)
+let fresh () =
+  TVar (Info.M ("what do I put here?"), None, "a fresh variable")
+
+let cunion =
+  List.fold_left (ConstraintSet.union) ConstraintSet.empty
+
+(* TODO(astory): determine how best to do this *)
+let munion' info map1 map2 =
+  let add_item k v map =
+    if StringMap.mem k map && StringMap.find k map <> v then
+      raise (TypeException (info, "two different bindings?"))
+    else
+      StringMap.add k v map
+  in
+  StringMap.fold add_item map1 map2
+
+let munion info =
+  List.fold_left (munion' info) StringMap.empty
+
 (* for now, just return type of underlying expression.  Later, need to modify ast*)
-let rec typecheck_exp (gamma, constraints) exp =
-  match exp with
+let rec typecheck_exp gamma expr =
+  match expr with
     | EVar (info, id) ->
       let s = Id.string_of_t id in
       if StringMap.mem s gamma then
-        (gamma, constraints, StringMap.find s gamma)
+        (gamma, ConstraintSet.empty, StringMap.find s gamma)
       else
-        raise TypeException(info, "Unbound value " ^ s)
-    | EApp (info, exp1, exp2) -> (gamma, constraints, TUnit)
-    | EFun (info, param, exp) -> (gamma, constraints, TUnit)
-    | ELet (info, bind, exp) -> (gamma, constraints, TUnit)
-    | EAsc (info, exp, typ) -> (gamma, constraints, TUnit)
-    | EOver (info, op, exps) -> (gamma, constraints, TUnit)
-    | EPair (info, exp1, exp2) -> (gamma, constraints, TUnit)
-    | ECase (info, exp1, pat_exps) -> (gamma, constraints, TUnit)
-    | EUnit (info) -> (gamma, constraints, TUnit)
-    | EInteger (info, value) -> (gamma, constraints, TUnit)
-    | EChar (info, value) -> (gamma, constraints, TUnit)
-    | EString (info, value) -> (gamma, constraints, TUnit)
-    | EBool (info, value) -> (gamma, constraints, TUnit)
+        raise (TypeException (info, "Unbound value " ^ s))
+    | EApp (info, expr1, expr2) ->
+      let resultant_type = fresh () in
+      let (gamma1, constraints1, typ1) =
+          typecheck_exp gamma expr1 in
+      let (gamma2, constraints2, typ2) =
+          typecheck_exp gamma expr2 in
+      let constraints' =
+          cunion [
+              constraints1;
+              constraints2;
+              ConstraintSet.singleton
+                  (typ1, TFunction (typ2, resultant_type))]
+      in
+      let gamma' = munion info [gamma1; gamma2] in
+      (gamma', constraints', resultant_type)
+    | EFun (info, param, expr) -> (gamma, ConstraintSet.empty, TUnit)
+    | ELet (info, bind, expr) -> (gamma, ConstraintSet.empty, TUnit)
+    | EAsc (info, expr, typ) -> (gamma, ConstraintSet.empty, TUnit)
+    | EOver (info, op, exprs) -> (gamma, ConstraintSet.empty, TUnit)
+    | EPair (info, expr1, expr2) -> (gamma, ConstraintSet.empty, TUnit)
+    | ECase (info, expr1, pat_exprs) -> (gamma, ConstraintSet.empty, TUnit)
+    | EUnit (info) -> (gamma, ConstraintSet.empty, TUnit)
+    | EInteger (info, value) -> (gamma, ConstraintSet.empty, TUnit)
+    | EChar (info, value) -> (gamma, ConstraintSet.empty, TUnit)
+    | EString (info, value) -> (gamma, ConstraintSet.empty, TUnit)
+    | EBool (info, value) -> (gamma, ConstraintSet.empty, TUnit)
 
 let typecheck_decl (gamma, constraints) decl =
   match decl with
     | DLet (info, bind) ->
-      match bind with 
+      (match bind with 
         | Bind (info, pattern, typopt, exp) ->
           let (gamma, constraints, t) =
-              typecheck_exp (gamma, constraints) exp in
-          (gamma, constraints)
+              typecheck_exp gamma exp in
+          (gamma, constraints))
 
     | DType (info, ids, id, labels) ->
       (*TODO(astory)*)
